57edd10887ec5498926d702899f77d163dd3b0b3
George Stagas <gstagas@gmail.com>
2011-03-04 21:36:34 +0200

    Now also searches package path for extensions and cleans up paths afterwards

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 var crypto = require('x')('crypto')
   , math = require('x')('math')
+  , util = require('x')('util')
   
 console.log('crypto.hash(string, [algo], [encoding])', ':' , crypto.hash('hello'))
 console.log('math.multiply(a, b)', ':', math.multiply(5, 10))
+console.log('util.sprintf()', ':', util.sprintf("[%10s]", 'monkey'))
+util.printf("util.printf() : [%10s]", 'monkey')
--- x.js
+++ x.js
@@ -1,5 +1,8 @@
 module.exports = function(mod) {
-  require.paths.unshift(module.parent.filename.substr(0, module.parent.filename.lastIndexOf('/')))
-  return require('node_extensions/' + mod)
+  // first requiring script's path, then package's path
+  require.paths.unshift(module.parent.filename.substr(0, module.parent.filename.lastIndexOf('/')), __dirname)
+  var mod_obj = require('node_extensions/' + mod)
     (require(mod)) // this is below so we know the line of the failed module
+  require.paths = require.paths.slice(2) // undo our changes in require.paths
+  return mod_obj
 }
